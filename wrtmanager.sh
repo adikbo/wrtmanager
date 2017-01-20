09f4a29eb664ef334b930741879eec1a350618f2
adikbo <adam.bojarsk@gmail.com>
2017-01-20 21:09:47 +0100

    zmiany w wrtmanager.sh

--- wrtmanager.sh
+++ wrtmanager.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/expect -f
 
+<<<<<<< HEAD
 
+
+set ipki [open lista.txt r]
+
+stty -echo
+send_user -- "Please enter the password for the root user of the devices: "
 expect_user -re "(.*)\n"
 send_user "\n"
 stty echo
@@ -10,10 +16,13 @@ set pass $expect_out(1,string)
 while {[gets $ipki ip] != -1} {
 
 spawn ssh root@$ip
-
+=======
 spawn ssh root@192.168.1.1
+>>>>>>> parent of 970ca6f... hasło podawane na początku jako zmienna pass
 
 expect "password: "
+
+send "ad10bxcv3\r"
 
 expect "# "
 
